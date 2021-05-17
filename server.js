5e0f22760901bdd57d07f510e3dad5460ed20734
James Corcoran <jpcsoccer1234@gmail.com>
2021-05-17 19:01:06 -0400

    added schema, began to write a get route for homepage

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
+// const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers'); // why this
+// const helpers = require('./utils/helpers'); // if used add this into create function 
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store); // dont know what this is
+ const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store); // dont know what this is
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers }); // why this 
+const hbs = exphbs.create(); 
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
