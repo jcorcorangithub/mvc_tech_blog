291311131d94fe9edb53def8da1419dd5583972f
James Corcoran <jpcsoccer1234@gmail.com>
2021-05-18 18:31:12 -0400

    edited two handlebars file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,34 +3,27 @@ const { Post } = require('../models');
 //const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-//   try {
-//     // Get all projects and JOIN with user data
-//     const projectData = await Project.findAll({
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//         },
-//       ],
-//     });
+  try {
+    const postData = await Post.findAll();
 
-//     // Serialize data so the template can read it
-//     const projects = projectData.map((project) => project.get({ plain: true }));
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts);
 
-//     // Pass serialized data and session flag into template
-//     res.render('homepage', { 
-//       projects, 
-//       logged_in: req.session.logged_in 
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      posts, 
+      logged_in: true 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
 //write route to send the data from the table -- use .json
-//on server.js conntect view(homepage) and layout directories
+//on server.js connect view(homepage) and layout directories
 //render data using handlebars, not just json
 
-res.json({hello: 'hello'});
+//res.json({hello: 'hello'});
 });
 
 // router.get('/project/:id', async (req, res) => {
