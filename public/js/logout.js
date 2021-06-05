e269608a04a2a27f6f2fb4ff11578c5f02ff06b7
James Corcoran <jpcsoccer1234@gmail.com>
2021-06-04 23:29:05 -0400

    deleted users word in fetch file path

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,19 +1,35 @@
-const logoutButton = document.querySelector('#logout');
+// const logoutButton = document.querySelector('#logout');
+
+// const logout = async () => {
+//     const response = await fetch('/api/users/logout', {
+//         // i dont know if users needs to be there 
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         }
+//     })
+
+//     if (response.ok) {
+//         document.location.replace('/');
+//     } else {
+//         alert("Server error: Please try again");
+//     }
+// }
+
+// logoutButton.addEventListener("click", logout);
 
 const logout = async () => {
-    const response = await fetch('/api/users/logout', {
-        // i dont know if users needs to be there 
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    })
-
+    const response = await fetch('/api/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
     if (response.ok) {
-        document.location.replace('/');
+      document.location.replace('/');
     } else {
-        alert("Server error: Please try again");
+      alert(response.statusText);
     }
-}
-
-//logoutButton.addEventListener("click", logout);
+  };
+  
+  document.querySelector('#logout').addEventListener('click', logout);
+  
