a2eb23b33d9eccbb6ffc94aaabcd3ba9e1172061
James Corcoran <jpcsoccer1234@gmail.com>
2021-06-05 21:51:17 -0400

    needed to change name to user_name on signup.js file

--- controllers/api/signUpRoute.js
+++ controllers/api/signUpRoute.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { User } = require("../../models");
 //const withAuth = require('../utils/auth');
 
-router.post("/signup", async (req, res) => {
+router.post("/signUp", async (req, res) => {
   try {
     const newUser = await User.create({
       user_name: req.body.user_name,
